dc973f82088d923cadfad0f0c0f8e56824160e8f
badgerops <github@badgerops.net>
2024-04-01 15:16:21 +0200

    added very  ugly frontend, working scheduling and listing jobs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module badgerops/dlserver
 
 go 1.21.5
 
-require github.com/mattn/go-sqlite3 v1.14.22 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/rs/cors"
 )
 
 /*
@@ -47,44 +49,90 @@ func main() {
 	})
 	fmt.Println("starting up mux for /jobs")
 	mux.HandleFunc("/getjobs", GetJobs)
-	mux.HandleFunc("/shedule", ScheduleJob)
+	mux.HandleFunc("/schedule", ScheduleJob)
 
+	// Set up the CORS middleware
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},                      // Allow all origins
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"}, // Allow these HTTP methods
+		AllowedHeaders: []string{"*"},                      // Allow all headers
+	})
+
+	// Wrap the mux with the cors handler
+	handler := c.Handler(mux)
 	// Start the server
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", handler)
 	fmt.Println("Web server started on port 8080")
 }
 
 func GetJobs(w http.ResponseWriter, r *http.Request) {
 	// Get the list of jobs
-	jobs := GetScheduledJobs()
-
+	jobs, err := GetScheduledJobs()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	// Write the response
+	// Set the content type to JSON and write the response
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(jobs)
 }
 
 func ScheduleJob(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	// Parse the request body
 	// mux.Body should look like {"name": "job name", "time": "time to run the job", "url": "file to download"}
 	// create a dummy io.reader for testing
-	testreader := strings.NewReader(`{"name": "job name", "time": "time to run the job", "url": "https://blog.badgerops.net/content/images/2020/03/badgemux.png"}`)
-	job := ParseJob(testreader)
+	//testreader := strings.NewReader(`{"name": "Foobar Job", "time": "1970-01-01 13:37:00", "url": "https://blog.badgerops.net/content/images/2020/03/badger.png"}`)
+	// first, convert the URL encoded string to a byte array
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	// then, convert the byte array to an io.Reader
+	r.Body = ioutil.NopCloser(bytes.NewReader(body))
+	// finally, parse the io.Reader
+	job := ParseJob(r.Body)
 
-	// Schedule the job
-	SavetoDB(job)
+	// check to see if the job already exists using CheckDupJobs, otherwise save the new job to the DB
 
-	// Write the response1
-	w.Write([]byte("Job scheduled"))
+	existingJob, err := CheckDupJobs(job)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if existingJob != nil {
+		// Write the response
+		w.Write([]byte(fmt.Sprintf("Existing job found: %s", existingJob)))
+	}
+	if existingJob == nil {
+		SavetoDB(job)
+		// Write the response
+		w.Write([]byte(fmt.Sprintf("Job %s scheduled", job.Name)))
+	}
 }
 
-func GetScheduledJobs() []byte {
+func GetScheduledJobs() ([]byte, error) {
 	// Get the list of jobs
 	jobs := GetJobsFromDB()
 
 	// Convert the jobs to a JSON response
-	jobsJSON, _ := json.Marshal(jobs)
+	jobsJSON, err := json.Marshal(jobs)
+	if err != nil {
+		return nil, err
+	}
 
-	return jobsJSON
+	return jobsJSON, nil
 }
+
+// func GetScheduledJobs() []byte {
+// 	// Get the list of jobs
+// 	jobs := GetJobsFromDB()
+
+// 	// Convert the jobs to a JSON response
+// 	jobsJSON, _ := json.Marshal(jobs)
+
+// 	return jobsJSON
+// }
 
 func ParseJob(body io.Reader) Job {
 	// Parse the JSON body
@@ -106,6 +154,20 @@ func GetJobsFromDB() []Job {
 	db.Close()
 
 	return jobs
+}
+
+// check for duplicate jobs
+func CheckDupJobs(job Job) (*Job, error) {
+	jobs := GetJobsFromDB()
+
+	// Search for the job in the list of jobs
+	for _, j := range jobs {
+		if j.Name == job.Name {
+			return &job, fmt.Errorf("%s", job.Name)
+		}
+	}
+
+	return nil, nil
 }
 
 func SavetoDB(job Job) {
